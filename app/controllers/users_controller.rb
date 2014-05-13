3b40cf0542240025c7d0883254dc7bbb2fe1d21d
Bernd Clausen <avs@bclausen.de>
2014-05-13 16:43:15 +0200

    Änderung_10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,19 @@ class UsersController < ApplicationController
 	end
 
 	def post
+		nick = params[:user][:nickname]
+		pw = params[:user][:password]
+		pw_conf = params[:user][:password_confirmation]
+		if User.new(:nickname=> nick,:password=> pw).authenticate pw_conf then
+			User.new(:nickname=> nick,:password=> pw).save
+		else
 
+			User.new(:nickname=> nick,:password=> pw).save
+			# @user = user
+		end
+		redirect_to users_path
 	end
+
 	# def new
 	# 	@users=User.new
 	# end
