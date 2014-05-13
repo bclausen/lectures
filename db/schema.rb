a75cc5d0745ca950f7c0685e348961f6ca849e56
ulf Kiesbye <ulf.kiesbye@web.de>
2014-05-13 14:28:45 +0200

    Änderung_8

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "lecturers", force: true do |t|
     t.string "lastname"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "users", force: true do |t|
     t.string "nickname"
+    t.string "password_digest"
   end
 
   add_index "users", ["nickname"], name: "index_users_on_nickname", unique: true
