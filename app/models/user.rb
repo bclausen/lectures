b1ac6cc09e351f5dd788e443f6fb1e9dc9fb173a
ulf Kiesbye <ulf.kiesbye@web.de>
2014-05-13 14:41:29 +0200

    Änderung_9

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
         presence: true,
         uniqueness: { case_sensitive: false }
 
-    before_save { self.nickname = nickname.downcase }
+        before_save { self.nickname = nickname.downcase }
+        
+ 	has_secure_password
+        validates :password, length: { minimum: 6 }
 end
